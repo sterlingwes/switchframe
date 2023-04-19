bd360226f712a76e595a19902cc2b57dde2c7798
Wes Johnson <wes@swj.io>
2023-04-19 08:19:10 -0400

    migrate site

---
+++ public/wp-content/themes/bridge/css/style_dynamic_responsive_callback.php
@@ -0,0 +1,59 @@
+@media only screen and (max-width: 1000px){
+						.content .container .container_inner.default_template_holder,
+		.content .container .container_inner.page_container_inner {
+			padding-top:80px !important; /*important because of the inline style on page*/
+		}
+	}
+@media only screen and (min-width: 480px) and (max-width: 768px){
+	
+	        section.parallax_section_holder{
+			height: auto !important;
+			min-height: 400px;
+		}
+		}
+@media only screen and (max-width: 768px){
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+@media only screen and (max-width: 600px) {
+	
+			h2, h2 a {
+			font-size: 27px; 			line-height: 33px; 		    		}
+	
+	
+			h4, h4 a {
+			font-size: 15px; 			line-height: 27px; 		    		}
+	
+	
+	
+			body,
+		p {
+			font-size: 13px; 					    		}
+	
+	
+	
+	
+	}
+@media only screen and (max-width: 480px){
+
+			section.parallax_section_holder {
+			height: auto !important;
+			min-height: 400px;
+		}
+	
+				.footer_bottom {
+			line-height: 23px;
+		}
+	
+	}
